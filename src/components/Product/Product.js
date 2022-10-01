2ad5a03f02bc626a177465e36c89aed22cd65188
TasneemLehaj <tasneemlehaj20@gmail.com>
2022-10-02 00:17:26 +0600

    added a new cart component

---
+++ src/components/Product/Product.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Product.css';
+
+const Product = (props) => {
+    // console.log(props.product)
+    const { handleAddToCart } = props;
+    const { img, name, price, seller, ratings } = props.product;
+    console.log(props);
+
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price:${price}</p>
+                <p><small>Seller:{seller}</small></p>
+                <p><small>Ratings:{ratings} stars</small></p>
+            </div>
+            <button onClick={() => props.handleAddToCart(props.product)} className='btn-cart'>
+                <p>Add To Cart</p>
+            </button>
+        </div>
+    );
+};
+
+export default Product;
